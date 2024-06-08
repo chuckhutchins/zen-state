d4c7ed034341e9da42e0e5d46d995ba30be8ac81
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-06-08 09:08:34 -0400

    adding todo items

--- src/views/MeditationView.vue
+++ src/views/MeditationView.vue
@@ -28,6 +28,15 @@ const handleChangeLength = (length: string) => {
 
 // TODO: persist meditation length in indexeddb
 // TODO: dynamically pull in the meditation type and all that
+// TODO: keep screen awake
+// TODO: find a way to keep streaming audio when screen turns off
+// TODO: figure out an audio player style
+// TODO: look into a simple, calming animation
+// TODO: need to ditch the black theme
+// TODO: change mission to look like a rock zen garden
+// TODO: change hr to be a tilde squiggle
+// TODO: change header/footer hr to slowing fade out
+// TODO: change mission text on refresh
 </script>
 
 <style scoped lang="scss">
