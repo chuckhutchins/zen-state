498228a6e77b486f500626804c67588d4dec66d4
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-07-20 15:09:53 -0400

    updating home page

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,21 +1,24 @@
 <template>
   <main class="main">
     <div class="mission">
-      <h1>You need to calm down. You're being too loud.</h1>
+      <h1>Breathe in. Breathe out.</h1>
     </div>
     <TheHr size="small" />
-    <div class="meditations">
-      <ul class="list">
-        <li>Breathe in</li>
-        <li>Breathe out</li>
-        <li><router-link :to="{name: 'MeditationPage'}">Guided</router-link></li>
-      </ul>
+    <div v-for="type in meditationList" class="type-list" :key="type.id">
+      <h2>{{ type.name }}</h2>
+      <CategoryList :typeId="type.id" />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
+import { useMeditationStore } from '@/stores/MeditationStore';
+import { storeToRefs } from 'pinia';
+import CategoryList from '@/components/category/CategoryList.vue';
 import TheHr from '@/components/common/TheHr.vue';
+
+const meditationStore = useMeditationStore();
+const { meditationList } = storeToRefs(meditationStore);
 </script>
 
 <style scoped lang="scss">
@@ -38,9 +41,10 @@ import TheHr from '@/components/common/TheHr.vue';
     }
   }
 
-  .meditations {
-    display: grid;
-    gap: 1rem;
+  .type-list {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
   }
 }
 </style>
