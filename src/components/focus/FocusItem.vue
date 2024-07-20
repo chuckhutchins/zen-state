e459569f8cf553ecc0092f7278583f70c81c3e50
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-07-20 15:05:16 -0400

    adding focus-related components

---
+++ src/components/focus/FocusItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <RouterLink :to="{ name: 'FocusPage', params: routeParams }" class="focus-item">
+    {{ focus.name }}
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const { focus } = defineProps(['focus']);
+
+const routeParams = ref({ focusId: focus.id });
+
+// TODO: find a nice complimentary color palette
+</script>
+
+<style scoped lang="scss">
+.focus-item {
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  padding: 1rem;
+  background-color: var(--color-sand);
+  border-block-end: none;
+  border-radius: 0.125rem;
+  transition: background-color .2s;
+  block-size: 5rem;
+
+  &:hover {
+    background-color: var(--color-sand-dark);
+  }
+}
+</style>
---
+++ src/components/focus/FocusList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="hasFocusList" class="focus-list">
+    <FocusItem
+      v-for="focus in focusList"
+      :focus="focus"
+      :key="focus.id"
+    />
+  </div>
+  <div v-else>
+    <p>There are no areas of focus in this category. If you focus hard enough, maybe the paths will reveal themselves to you.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import FocusItem from '@/components/focus/FocusItem.vue';
+
+const { focusList } = defineProps(['focusList']);
+
+const hasFocusList = computed(() => focusList.length > 0);
+</script>
+
+<style scoped lang="scss">
+.focus-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+  gap: 1rem;
+}
+</style>
