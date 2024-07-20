5596954f191cb1dd897e7e081db3f937dfcd47de
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-07-20 15:03:50 -0400

    adding category-related components

---
+++ src/components/category/CategoryItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <RouterLink :to="{ name: 'CategoryPage', params: routeParams }" class="category-item">
+    {{ categoryName }}
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+const { category, typeId } = defineProps(['category', 'typeId']);
+
+const routeParams = ref({ categoryId: category.id, typeId });
+const categoryName = computed(() => category.name);
+
+// TODO: find a nice complimentary color palette
+</script>
+
+<style scoped lang="scss">
+.category-item {
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  padding: 1rem;
+  background-color: var(--color-sand);
+  border-block-end: none;
+  border-radius: 0.125rem;
+  transition: background-color .2s;
+  block-size: 5rem;
+
+  &:hover {
+    background-color: var(--color-sand-dark);
+  }
+}
+</style>
---
+++ src/components/category/CategoryList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="hasCategoryList" class="category-list">
+    <CategoryItem
+      v-for="category in categoryList"
+      :category="category"
+      :typeId="typeId"
+      :key="category.id"
+    />
+  </div>
+  <div v-else>
+    <p>There are no categories in this type. Categorize your distractions and return to the present moment.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useMeditationStore } from "@/stores/MeditationStore.js";
+import CategoryItem from '@/components/category/CategoryItem.vue';
+
+const meditationStore = useMeditationStore();
+const { typeId } = defineProps(['typeId']);
+
+const hasCategoryList = computed(() => categoryList.value.length > 0);
+const categoryList = computed(() => meditationStore.getCategoryList(typeId));
+</script>
+
+<style scoped lang="scss">
+.category-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+  gap: 1rem;
+}
+</style>
