3630d97c1f7eadc9d0674799366f6444d4025654
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-07-20 11:40:25 -0400

    adding meditations meta data

---
+++ src/assets/data/meditations.ts
@@ -0,0 +1,61 @@
+export const meditations = [
+  {
+    type: 'Singles',
+    categoryList: [
+      {
+        name: 'Classics',
+        focusList: [
+          {
+            name: 'Guided',
+            fileList: [
+              10,
+              15,
+              20,
+              30,
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'Courses',
+    categoryList: [
+      {
+        name: 'Foundation',
+        focusList: [
+          {
+            name: 'Basics',
+            fileList: [
+              1,
+              2,
+              3,
+              4,
+              5,
+              6,
+              7,
+              8,
+              9,
+              10,
+            ]
+          },
+          {
+            name: 'Basics 2',
+            fileList: [
+              1,
+              2,
+              3,
+              4,
+              5,
+              6,
+              7,
+              8,
+              9,
+              10,
+            ]
+          },
+        ]
+      }
+    ]
+  }
+];
